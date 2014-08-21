24119afbdbdfe05530f548e4db3e57341533d608
lhl <lhl@randomfoo.net>
2014-08-21 18:49:48 +0200

    Python HelloWorld

---
+++ 001-pythonsdk/README.md
@@ -0,0 +1,5 @@
+INSTALL:
+pip install python-ovrsdk
+
+DOCS:
+https://github.com/jherico/python-ovrsdk/tree/feature/linuxsupport
---
+++ 001-pythonsdk/demo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import time
+from ovrsdk import *
+
+ovr_Initialize()
+hmd = ovrHmd_Create(0)
+hmdDesc = ovrHmdDesc()
+ovrHmd_GetDesc(hmd, byref(hmdDesc))
+
+print hmdDesc.ProductName
+
+ovrHmd_StartSensor(hmd,
+                   ovrSensorCap_Orientation | ovrSensorCap_YawCorrection,
+		   0
+		  )
+
+while True:
+  ss = ovrHmd_GetSensorState(hmd, ovr_GetTimeInSeconds())
+  pose = ss.Predicted.Pose
+  print "%10f   %10f   %10f   %10f" % ( \
+    pose.Orientation.w,
+    pose.Orientation.x,
+    pose.Orientation.y,
+    pose.Orientation.z
+  )
+  time.sleep(0.016)
