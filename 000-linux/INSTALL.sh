434f40a11e9c9d0539da4c3a0d605e8a9cf8b0ab
lhl <lhl@randomfoo.net>
2014-08-21 20:11:45 +0200

    some packages

---
+++ 000-linux/INSTALL.sh
@@ -0,0 +1,44 @@
+##
+# Base setup is from https://help.ubuntu.com/community/Installation/MinimalCD
+# Ubuntu 14.04 amd64 USB boot stick
+# * Basic Server
+# * OpenSSH Server
+# * Extra Fonts
+# * ubuntu-desktop
+##
+
+# Basics
+sudo apt-get install -y avahi-daemon
+sudo apt-get install -y unzip
+sudo apt-get install -y build-essential
+sudo apt-get install -y git
+
+# Python
+sudo apt-get install -y python-setuptools
+sudo easy_install pip
+
+
+# nvidia drivers
+sudo add-apt-repository ppa:xorg-edgers/ppa -y
+sudo apt-get update
+sudo apt-get install -y nvidia-343
+
+
+# X stuff
+sudo apt-get install -y chromium-browser
+sudo apt-get install -y vlc
+sudo apt-add-repository ppa:tycho-s/ppa -y
+sudo apt-get update
+sudo apt-get install -y qtile
+
+# nodm - future use
+sudo apt-get install -y nodm
+
+###
+# See also: https://randomfoo.hackpad.com/Linux-Desktop-Tools-QFiFiyW2Wmv
+###
+
+# sudo apt-get install i3
+# sudo apt-get install mrxvt
+# sudo apt-get install stress
+
