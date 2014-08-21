f49515b5eb538ddb3bbcdc2ac3cd654ea7726b4b
lhl <lhl@randomfoo.net>
2014-08-21 18:56:13 +0200

    formatting

--- 001-pythonsdk/demo.py
+++ 001-pythonsdk/demo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import time
-from ovrsdk import *
+from   ovrsdk import *
 
 ovr_Initialize()
 hmd = ovrHmd_Create(0)
@@ -18,10 +18,9 @@ ovrHmd_StartSensor(hmd,
 while True:
   ss = ovrHmd_GetSensorState(hmd, ovr_GetTimeInSeconds())
   pose = ss.Predicted.Pose
-  print "%10f   %10f   %10f   %10f" % ( \
-    pose.Orientation.w,
-    pose.Orientation.x,
-    pose.Orientation.y,
-    pose.Orientation.z
-  )
+  print "%10f   %10f   %10f   %10f" % (pose.Orientation.w,
+		                       pose.Orientation.x,
+				       pose.Orientation.y,
+				       pose.Orientation.z
+		                      )
   time.sleep(0.016)
